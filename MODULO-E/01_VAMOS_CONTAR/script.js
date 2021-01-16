a9b8d32048e8e2e737c04bd6d6ca100cbd1c95c8
Felipe Moraes <em.felipe@gmail.com>
2021-01-16 09:25:16 -0300

    finalizado tratamentos de entrada de dados

--- MODULO-E/01_VAMOS_CONTAR/script.js
+++ MODULO-E/01_VAMOS_CONTAR/script.js
@@ -3,21 +3,27 @@ function contar() {
     let fim = document.getElementById('fim') 
     let passo = document.getElementById('passo') 
 
-    let resultado = document.getElementById('resultado')    
+    let resultado = document.getElementById('resultado') 
+    
+    resultado.style.textAlign = 'center'
+    resultado.style.padding = '10px'
 
     if (inicio.value.length == 0 || fim.value.length == 0 || fim.value.length == 0) {
-        resultado.innerHTML += `IMPOSSÍVEL CONTAR \u{1F3C1}`
+        resultado.innerHTML = `IMPOSSÍVEL CONTAR \u{1F3C1}`
         window.alert('[ERRO] Existem Não Preenchidos!')
     } else {
 
         resultado.innerHTML = `Contando: <br>`
 
-        resultado.style.textAlign = 'center'
-        resultado.style.padding = '10px'
-
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value) 
+
+        //verificando passo = 0
+        if (p <= 0) {
+            window.alert(`${p} é um passo inválido! Considerando PASSO = 1`)
+            p = 1
+        }
         
         if (i < f) {
             //contagem crescente
@@ -26,12 +32,10 @@ function contar() {
             }
         } else  {
             //contagem decrescente
-            for (let c = i; c <= f; c -= p) {
+            for (let c = i; c >= f; c -= p) {
                 resultado.innerHTML += `${c} \u{1F449}`
             }    
-        }
-        
-        
+        }       
 
         resultado.innerHTML += `FIM! \u{1F3C1}`
     }    
