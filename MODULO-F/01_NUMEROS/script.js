840770ce487066e9546e9a562f3270d55f83399f
Felipe Moraes <em.felipe@gmail.com>
2021-01-19 22:53:40 -0300

    feito resolução final do exercicio

--- MODULO-F/01_NUMEROS/script.js
+++ MODULO-F/01_NUMEROS/script.js
@@ -28,7 +28,8 @@ function adicionar() {
         valores.push(Number(inputNum.value))
         let item = document.createElement('option')
         item.text = `Número ${inputNum.value} adicionado.`
-        numList.appendChild(item)
+        numList.appendChild(item) //adicionando na lista visual
+        resultado.innerHTML = '' //limpando informações do display
     } else {
         alert(`${inputNum.value} Valor inválido ou já adicionado na lista.`)
     }
@@ -41,10 +42,39 @@ function analisar() {
     if(valores.length == 0) {
         alert('Lista Vazia! Adicione valores na lista.')
     } else {
-        let totalElementos = valores.length
+        
+        let totalElementos = valores.length //pegando o total de elementos
+
+        //pegando o maior e menor elementos
+        let maior = valores[0]
+        let menor = valores[0]
+
+        for(let i in valores) {
+            //verifica maior
+            if(valores[i] > maior) {
+                maior = valores[i]
+            }
+            //verifica menor
+            if(valores[i] < menor) {
+                menor = valores[i]
+            }
+        }
+
+        //pegando a soma e média
+        let soma = 0
+        for(let i in valores) {
+            soma+=valores[i]
+        }
+
+        //calculando a média
+        let media = soma / totalElementos
         
         resultado.innerHTML = ''
-        resultado.innerHTML += `O total de elementos é ${totalElementos}`
-        
+        resultado.innerHTML += `<p>O total de elementos é ${totalElementos}</p>`
+        resultado.innerHTML += `<p>O maior valor informado é ${maior}</p>`
+        resultado.innerHTML += `<p>O menor valor informado é ${menor}</p>`
+        resultado.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        resultado.innerHTML += `<p>A médio dos valores é ${media}</p>`
+
     }
 }
